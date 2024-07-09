2887ef3ddb7405f759fba047eac1e1b8d1a35ecc
osam7a <osamaziadalhinawi@gmail.com>
2024-07-09 17:33:06 +0400

    remove whitenoise from middlewhere

--- core/settings.py
+++ core/settings.py
@@ -48,7 +48,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.security.SecurityMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
+    # "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
