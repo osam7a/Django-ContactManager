5ac50274d78ed1b2f9c5f0d45d4876fbfe5c8e3d
osam7a <osamaziadalhinawi@gmail.com>
2024-07-09 16:33:33 +0400

    add whitenoise (for testing)

--- core/settings.py
+++ core/settings.py
@@ -49,8 +49,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
--- core/urls.py
+++ core/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 from . import views
 
@@ -25,3 +27,5 @@ urlpatterns = [
 
     path('advanced_filters/', include('advanced_filters.urls'))
 ]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
