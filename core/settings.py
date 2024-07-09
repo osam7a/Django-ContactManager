e9f75a1189094a453b44830b9e8d6edfe47f5936
osam7a <osamaziadalhinawi@gmail.com>
2024-07-09 16:42:24 +0400

    checking whitnoise for static files

--- core/settings.py
+++ core/settings.py
@@ -148,8 +148,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR / 'static'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
