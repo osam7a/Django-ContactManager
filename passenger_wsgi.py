87e332c1021a638a4a2c9accea116590ac5cac46
Osama Alhennawi <osamaziadalhinawi@gmail.com>
2024-07-09 23:19:39 +0400

    use environment variable PYTHON_INTERPRETER

--- passenger_wsgi.py
+++ passenger_wsgi.py
@@ -1,6 +1,7 @@
 import sys, os
 
-INTERP = os.environ['HOME'] + '/repositories/Django-ContactManager/venv/bin/python'
+INTERP = os.environ.get('PYTHON_INTERPRETER', 0)
+if INTERP == 0: sys.stderr.write("No PYTHON_INTERPRETER found\n"); sys.exit(1)
 if sys.executable != INTERP: os.execl(INTERP, INTERP, *sys.argv)
 
 from core.wsgi import application
