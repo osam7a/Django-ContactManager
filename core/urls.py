bb4ae713384e0c45f16f23f32abbc8445dc3caa8
osam7a <osamaziadalhinawi@gmail.com>
2024-07-09 15:20:26 +0400

    Added basic index view to identify server status

--- core/urls.py
+++ core/urls.py
@@ -17,8 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from . import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 
     path(r'^advanced_filters/', include('advanced_filters.urls'))
 ]
